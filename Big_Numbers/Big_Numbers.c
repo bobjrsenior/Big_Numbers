6d70604852bc65b4b9654a36841fbe958f29e0dd
Bobjrsenior <bobjrsenior@gmail.com>
2016-03-21 17:31:00 -0400

    Added a few more comments. Now deals with remainders greater than 1. Doesn't handle when an individual position has an integer overflow

--- Big_Numbers/Big_Numbers.c
+++ Big_Numbers/Big_Numbers.c
@@ -25,24 +25,32 @@ void addIntToBig(Big_Number* bigNumber, int number){
 		length = (4 + bigNumber->number[2]);
 	int remainder = 0;
 
+	//Go through each position in the Big_Number
 	for (; cur < length; ++cur){
+		//Add the number and remainder
 		bigNumber->number[cur] += (number + remainder);
-		if (bigNumber->number[cur] >= 1000000000){
-			bigNumber->number[cur] -= 1000000000;
-			number = 0;
-			remainder = 1;
-		}
-		else{
 
+		//If there is an overflow, send it to the next position
+		if (bigNumber->number[cur] >= 1000000000){
+			remainder = bigNumber->number[cur] / 1000000000;
+			bigNumber->number[cur] -= (1000000000 * remainder);
+			number = 0;
+			
+		}//If there is no overflow, we are done
+		else{
 			remainder = 0;
 			break;
 		}
 	}
+
+	//If we reached the end and had a remainder
 	if (cur == length && remainder != 0){
+		//Make sure we don't have a buffer overflow
 		if (length == maxLength){
 			return;
 		}
 		else{
+			//Add the final remainder to the end and increment our size
 			bigNumber->number[cur] = remainder;
 			++bigNumber->number[2];
 		}
@@ -53,8 +61,12 @@ void printBigNumber(Big_Number* bigNumber){
 	int cur = (4 + bigNumber->number[1]),
 		length = (4 + bigNumber->number[2]) - 1;
 
+	//Printing backwards since array stores number from leat to most significant
+
+	//Print the first number without leading zeros
 	printf("%d", bigNumber->number[length]);
 
+	//Print the rest with leading zeros
 	for (--length; length >= cur; --length){
 		printf("%09d", bigNumber->number[length]);
 	}
--- Big_Numbers/main.c
+++ Big_Numbers/main.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]){
 	addIntToBig(bigNum, 5);
 	printBigNumber(bigNum);
 	subIntFromBig(bigNum, 3);
-	addIntToBig(bigNum, 900000000);
+	addIntToBig(bigNum, 2000000000);
 	printBigNumber(bigNum);
 	addIntToBig(bigNum, 100000000);
 	printBigNumber(bigNum);
